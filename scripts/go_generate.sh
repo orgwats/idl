db1f673d8ec52c422e60dc1892db5bcdc8be6232
waik1ki <arbutus@outlook.kr>
2025-03-19 00:16:59 +0900

    chore: proto generate 로직 수정

--- scripts/go_generate.sh
+++ scripts/go_generate.sh
@@ -8,10 +8,18 @@ OUT_DIR="../gen/go"
 
 mkdir -p "${OUT_DIR}"
 
-protoc \
-  -I="${PROTO_DIR}" \
-  --go_out="${OUT_DIR}" \
-  --go-grpc_out="${OUT_DIR}" \
-  --go_opt=paths=source_relative \
-  --go-grpc_opt=paths=source_relative \
-  $(find "${PROTO_DIR}" -name "*.proto")
+while IFS= read -r -d '' PROTO_SUBDIR; do
+  PROTO_FILES=$(find "${PROTO_SUBDIR}" -maxdepth 1 -name "*.proto")
+  if [ -n "${PROTO_FILES}" ]; then
+    SUBDIR_NAME=$(basename "${PROTO_SUBDIR}")
+    OUT_SUBDIR="${OUT_DIR}/${SUBDIR_NAME}"
+    mkdir -p "${OUT_SUBDIR}"
+    protoc \
+      -I="${PROTO_SUBDIR}" \
+      --go_out="${OUT_SUBDIR}" \
+      --go-grpc_out="${OUT_SUBDIR}" \
+      --go_opt=paths=source_relative \
+      --go-grpc_opt=paths=source_relative \
+      ${PROTO_FILES}
+  fi
+done < <(find "${PROTO_DIR}" -type d -print0)
